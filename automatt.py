d9d36a795498ce1ac2fa5aa3288315b41ff1f94f
Parker Higgins <parker@parkerhiggins.net>
2024-11-18 16:09:46 -0500

    fall back to updated time if published time not available

--- automatt.py
+++ automatt.py
@@ -242,7 +242,9 @@ def handle_rss_feed(site):
 
     new_posts = [entry for entry in f.entries 
                  if entry and time.mktime(time.gmtime()) - 
-                 time.mktime(entry.get('published_parsed')) <= (86400 * 1 + 120)]
+                 time.mktime(entry.get('published_parsed',
+                                       entry.get('updated_parsed')))
+                 <= (86400 * 1 + 120)]
 
     for entry in new_posts:
         record = {}
